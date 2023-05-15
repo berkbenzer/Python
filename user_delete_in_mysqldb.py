c9b2ce1046074113b0a793b696982c73b5ea3121
Berk Benzer <52455783+berkbenzer@users.noreply.github.com>
2023-05-15 16:26:47 +0200

    Update user_delete_in_mysqldb.py

--- user_delete_in_mysqldb.py
+++ user_delete_in_mysqldb.py
@@ -5,48 +5,72 @@ import getpass
 
 # Connect to database
 
-conn = mysql.connector.connect(
+def establish_connection():
+    conn = mysql.connector.connect(
+        database='mysql',
+        user='root',
+        password='password'
+    )
+    return conn
 
-    database='mysql',
-    user='root',
-    password='db_password'
-)
 
+def close_connection(conn):
+    # Close the database connection
+    conn.close()
 
-# Prompt the user for a comma-separated list of usernames to delete
-usernames_input = raw_input("Enter comma-separated list of usernames to delete: ")
-usernames = [username.strip() for username in usernames_input.split(",")]
-
-# Delete each username from the database
-for username in usernames:
-    # Check if the user exists in the database
-    query = "SELECT user, host FROM mysql.user WHERE user = %s"
+def delete_user(conn, user_name, user_host):
+    # Create a cursor for executing the DROP USER query
     cursor = conn.cursor()
-    cursor.execute(query, (username,))
-    results = cursor.fetchall()
+    drop_query = "DROP USER %s@%s"
+    cursor.execute(drop_query, (user_name, user_host))
 
-    if results:
-        for result in results:
-            user_name = result[0]
-            user_host = result[1]
-            print("%s@'%s' found in the database" % (user_name, user_host))
+    # Commit the changes to the database
+    conn.commit()
+    cursor.close()
 
-            # Prompt for confirmation to delete the user
-            confirm = raw_input("Are you sure you want to delete %s@'%s'? (Y/N): " % (user_name, user_host))
+def prompt_confirmation(user_name, user_host):
+    # Prompt for confirmation to delete the user
+    confirm = raw_input("Are you sure you want to delete %s@'%s'? (Y/N): " % (user_name, user_host))
+    return confirm.lower() == "y"
 
-            if confirm.lower() == "y":
-                # Create a separate cursor for executing the DROP USER query
-                drop_cursor = conn.cursor()
-                drop_query = "DROP USER %s@%s"
-                drop_cursor.execute(drop_query, (user_name, user_host))
+def delete_usernames(conn, usernames):
+    # Delete each username from the database
+    for username in usernames:
+        # Check if the user exists in the database
+        query = "SELECT user, host FROM mysql.user WHERE user = %s"
+        cursor = conn.cursor()
+        cursor.execute(query, (username,))
+        results = cursor.fetchall()
 
-                # Commit the changes to the database
-                conn.commit()
-                print("%s@'%s' deleted from the database" % (user_name, user_host))
-            else:
-                print("%s@'%s' deletion canceled" % (user_name, user_host))
-    else:
-        print("%s not found in the database" % username)
+        if results:
+            for result in results:
+                user_name = result[0]
+                user_host = result[1]
+                print("%s@'%s' found in the database" % (user_name, user_host))
 
-# Close the database connection
-conn.close()
+                if prompt_confirmation(user_name, user_host):
+                    delete_user(conn, user_name, user_host)
+                    print("%s@'%s' deleted from the database" % (user_name, user_host))
+                else:
+                    print("%s@'%s' deletion canceled" % (user_name, user_host))
+        else:
+            print("%s not found in the database" % username)
+
+def main():
+    # Prompt the user for a comma-separated list of usernames to delete
+    usernames_input = raw_input("Enter comma-separated list of usernames to delete: ")
+    usernames = [username.strip() for username in usernames_input.split(",")]
+
+    # Establish a database connection
+    conn = establish_connection()
+
+    # Delete the usernames from the database
+    delete_usernames(conn, usernames)
+
+    # Close the database connection
+    close_connection(conn)
+
+# Entry point of the script
+if __name__ == "__main__":
+    main()
+
